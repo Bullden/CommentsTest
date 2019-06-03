b8a73c12ce30cca70aba93e760b1401740d18307
bullden <deniiserbull@gmail.com>
2019-06-03 18:48:19 +0300

    changes in murkup

--- script.js
+++ script.js
@@ -1,31 +1,65 @@
-const COMMENTS =[{
-    name: "Самуил",
-    data: "13 октября 2011",
-    text: "Привет, Верунь! ниче себе ты крутая. фотка класс!!!!"
-}, {
-    name: "Лилия Семёновна",
-    data: "14 октября 2011",
-    text: "Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент, это и есть всемирно известный центр огранки алмазов и торговли бриллиантами?"
-}, {
-    name: "Лилия Семёновна",
-    data: "14 октября 2011",
-    text: "Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент?"
-}, {
+const COMMENTS =[   {
     name: "Леонид",
     data: "20 декабря 2015",
     text: "Привет!"
 }, {
+    name: "Роман",
+    data: "13 декабря 2016",
+    text: "Услуги супер"
+}, {
+    name: "Григорий",
+    data: "13 декабря 2016",
+    text: "Ну ничего себе"
+},{
+    name: "Светлана",
+    data: "13 декабря 2016",
+    text: "Спасибо,вера"
+},{
+    name: "Анечка",
+    data: "13 декабря 2016",
+    text: "Мне не очень понравилось все"
+},{
+    name: "Татьяна",
+    data: "13 декабря 2016",
+    text: "Ну... мне эти ваши услуги лаже не понадобились. Я сама все умею"
+},{
+    name: "Костя",
+    data: "13 декабря 2016",
+    text: "Ты очень красивая,Верочка"
+},{
+    name: "Артем",
+    data: "13 декабря 2016",
+    text: "Это моя девушка"
+},{
+    name: "Соня",
+    data: "13 декабря 2016",
+    text: "1. Услуги класс,все правда понравилось \n" +
+        "2.Решение моей проблемы было быстрым и оперативным \n" +
+        "3.Все Супер! Буду обращаться еще \n"
+},{
     name: "Рома",
     data: "13 декабря 2016",
     text: "Фотка класс!=)"
-}, {
+},{
     name: "Игорь",
     data: "20 января 2017",
     text: "Нет на земле дороже,\n" +
          "Ласковых глаз твоих.\n" +
          "Сердце моё они тревожат,\n" +
          "Счастлив тонуть я в них."
-}];
+},{
+    name: "Самуил",
+    data: "13 октября 2011",
+    text: "Привет, Верунь! ниче себе ты крутая. фотка класс!!!!"
+},{
+    name: "Лилия Семёновна",
+    data: "14 октября 2011",
+    text: "Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент, это и есть всемирно известный центр огранки алмазов и торговли бриллиантами?"
+},{
+    name: "Лилия Семёновна",
+    data: "14 октября 2011",
+    text: "Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент?"
+},];
 
 
 function RenderComments(commentsArr) {
@@ -61,7 +95,7 @@ function RenderComments(commentsArr) {
     });
     document.getElementById('comments').innerHTML = "";
     document.getElementById('comments').appendChild(commentsContainer);
-    document.getElementById('comments-counter').innerText = commentsArr.length;
+    document.getElementById('comments-counter').innerText = COMMENTS.length;
 }
 RenderComments(COMMENTS.slice(-3));
 
@@ -84,13 +118,17 @@ document.getElementById("add-btn").addEventListener("click", (e) => {
 
 document.getElementById("all-reviews").addEventListener("click",(e) => {
     e.preventDefault();
+    document.getElementById("all-reviews").classList.toggle("newClassAll");
+    document.getElementById("last-reviews").classList.toggle("newClassLast");
     RenderComments(COMMENTS)
 });
 document.getElementById("last-reviews").addEventListener("click",(e) => {
     e.preventDefault();
+    document.getElementById("last-reviews").classList.toggle("newClassLast");
+    document.getElementById("all-reviews").classList.toggle("newClassAll");
     RenderComments(COMMENTS.slice(-3))
 });
-document.getElementById("like-counter").innerText = Math.floor(Math.random() * 100);
+/*document.getElementById("like-counter").innerText = Math.floor(Math.random() * 100);*/
 
 document.getElementById("textAdd")
     .addEventListener("keyup", function(event) {
@@ -99,5 +137,6 @@ document.getElementById("textAdd")
             document.getElementById("add-btn").click();
         }
     });
+
 
 
